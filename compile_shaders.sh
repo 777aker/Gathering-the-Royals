01bb7acca5f7ded645a3963170f142ddb153bcd2
Kelley <777aker@gmail.com>
2025-11-14 09:45:33 -0700

    working through a few things

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/home/kelley/vulkan/1.4.321.1/x86_64/bin/glslc ../shaders/simple_shader.vert -o ../shaders/simple_shader.vert.spv
-/home/kelley/vulkan/1.4.321.1/x86_64/bin/glslc ../shaders/simple_shader.frag -o ../shaders/simple_shader.frag.spv
+/home/kelley/Vulkan-SDK/1.4.328.1/x86_64/bin/glslc ../shaders/simple_shader.vert -o ../shaders/simple_shader.vert.spv
+/home/kelley/Vulkan-SDK/1.4.328.1/x86_64/bin/glslc ../shaders/simple_shader.frag -o ../shaders/simple_shader.frag.spv
--- deck builder builder/TODO.md
+++ deck builder builder/TODO.md
@@ -7,15 +7,18 @@ kanban-plugin: board
 ## TODO
 
 - [ ] Character Creator
+	- [ ] add shape models
+	- [ ] add shape options to left
+	- [ ] when click create shape and drag with cursor
+	- [ ] drop when released inside box area
+	- [ ] save shapes and parameters to file
+- [ ] Main Menu
 
 
 ## In Progress
 
 - [ ] Font
-	- [ ] click add point
-	- [ ] connect points
-	- [ ] ctrl z
-	- [ ] right click remove point
+	- [ ] add ttf reading
 
 
 ## Done
